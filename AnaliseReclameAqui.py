962f5ef1111cf871371181614daa5cd87670f1e1
miguel-victor-tic <60945692+miguel-victor-tic@users.noreply.github.com>
2024-10-08 00:59:26 -0300

    Update AnaliseReclameAqui.py

--- AnaliseReclameAqui.py
+++ AnaliseReclameAqui.py
@@ -1,9 +1,9 @@
-import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
 from plotly.subplots import make_subplots
+import streamlit as st
 
 st.title('Análise de Dados DeepLearn')
 
